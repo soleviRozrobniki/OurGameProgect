b856a0e657ad0e3c6bed90447667ae3fbc379700
Catsanio <b.pavlenko07@gmail.com>
2023-06-12 19:40:09 +0300

    Change?

---
+++ Assets/Scripts/EnemyBrain.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBrain : MonoBehaviour
+{
+
+    public float moveSpeed = 3f;
+
+    private Transform player;
+    private Rigidbody2D rb;
+
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        // Визначення напрямку руху до гравця
+        Vector2 direction = player.position - transform.position;
+
+        // Рух ворога в напрямку гравця
+        rb.velocity = direction.normalized * moveSpeed;
+    }
+}
---
+++ Assets/Scripts/TurretScript.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurretScript : MonoBehaviour
+{
+    public float rotationSpeed = 10f;
+    public GameObject bulletPrefab;
+    public Transform firePoint;
+    public float fireRate = 1f, bulletSpeed;
+
+    private Transform player;
+    private float fireCountdown = 0f;
+
+    private void Start()
+    {
+        
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+
+        // Запуск пострілів з моменту старту
+        fireCountdown = 1f / fireRate;
+        InvokeRepeating("Shoot", 3f,1 / fireRate);
+    }
+
+    private void Update()
+    {
+        // Визначення напрямку до гравця
+        Vector2 direction = player.position - transform.position;
+
+        // Обчислення кута обертання до гравця
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg - 180;
+
+        // Обертання турелі до гравця
+        //Quaternion rotation = Quaternion.Euler(angle, Vector3.forward);
+        transform.rotation = Quaternion.Euler(0, 0, angle);
+
+        // Логіка пострілів
+        //if (fireCountdown <= 0f)
+        
+            //Shoot();
+            //fireCountdown = 1f / fireRate;
+        
+
+        fireCountdown -= Time.deltaTime;
+    }
+
+    private void Shoot()
+    {
+        // Створення снаряду та вистріл
+        GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+        bullet.GetComponent<Rigidbody2D>().AddForce(-transform.right *
+            bulletSpeed);
+    }
+}
